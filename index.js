d95e393ba21e1f8a10668e160b456266b4db1b31
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-03-16 04:35:05 -0700

    successfully bring out the link musics from youtube

--- index.js
+++ index.js
@@ -1,32 +1,79 @@
 const Discord = require('discord.js');
-const dotenv = require('dotenv');
 const fetch = require('node-fetch');
+const dotenv = require('dotenv');
+const ytdl = require("ytdl-core");
 dotenv.config();
 const client = new Discord.Client();
 
-const getQuote = function(link) {
-  return fetch(link)
-    .then(res => {
-      console.log(res);
-      return res.json();
-    })
-    // .then(data => {
-    //   console.log(data);
-    //   return data[0]["q"] + " -" + data[0]["a"]
-    // })
-}
+const excute = async function(message, serverQueue) {
+  // console.log("serverQueue", serverQueue);
+  const args = message.content.split(" ");
+
+  const voiceChannel = message.member.voice.channel;
+  // console.log(message.member.voice.channel);
+  if (!voiceChannel) {
+    return message.channel.send(
+      "You need to be in the voice chat for playing music!"
+    );
+  }
+  const permissions = voiceChannel.permissionsFor(message, client.user);
+  // console.log("permission", permissions);
+  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+    return message.channel.send(
+      "I need the permissions to join and speak in your voice channel!"
+    );
+  }
+
+  const songInfo = await ytdl.getInfo(args[1])
+  const songs = {
+    title: songInfo.videoDetails.title,
+    url: songInfo.videoDetails.video_url
+  };
+  console.log(songs);
+
+  // if (!serverQueue) {
+
+  // } else {
+  //   serverQueue.songs.push(songs);
+  //   console.log("serverQ.song", serverQueue.songs);
+  //   return message.channel.send(`${songs.title} has added to the queue!`);
+
+  // }
+};
+
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("message", msg => {
-  if (msg.author.bot) return
+  //this is if the message is from the bot, then ignore.
+  if (msg.author.bot) return;
 
-  if (msg.content.includes('https')) {
-    console.log(msg.content)
-    getQuote(msg.content).then(quote => msg.channel.send(quote));
+  //if not starting with !, return
+  if (!msg.content.startsWith(process.env.prefix)) return;
+
+  const queue = new Map();
+
+  //A guild represents an isolated collection of users and channels and is often referred to as a server
+  const serverQueue = queue.get(msg.guild.id);
+
+  if (msg.content.startsWith(`${process.env.prefix}play`)) {
+    excute(msg,serverQueue);
   }
+  else if (msg.content.startsWith(`${process.env.prefix}skip`)) {
+    // skip (msg, serverQueue);
+    msg.channel.send("pressed skip");
+    return;
+  }
+  else if (msg.content.startsWith(`${process.env.prefix}stop`)) {
+    // stop (msg, serverQueue);
+    msg.channel.send("pressed stop");
+    return;
+  } else {
+    msg.channel.send('You need to enter a valid command!');
+  }
+
 });
 
 client.login(process.env.Token);
